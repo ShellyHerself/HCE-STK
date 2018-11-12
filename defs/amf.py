f390eb0055a6d98fa640416ab8de740c9617bdbc
gbMichelle <gbmichelle.dev@gmail.com>
2018-11-12 02:38:25 -0500

    Finished the AMF(Adjutant Model File) definition.

--- defs/amf.py
+++ defs/amf.py
@@ -1,64 +1,395 @@
 from reclaimer.common_descs import *
 from supyr_struct.defs.tag_def import TagDef
+import math
 
 
+def get_vert_node_count(rawdata=None, **kwargs):
+    indices = rawdata.peek(4)
+    if   indices[1] == 255: return 1
+    elif indices[2] == 255: return 2
+    elif indices[3] == 255: return 3
+    else: return 4
+
+def get_vert_node_weight_count(parent=None, **kwargs):
+    if parent.node_indices[-1] != 255:
+        return 4
+    return len(parent.node_indices) - 1
+
+def is_vert_count_over_65535(parent=None, **kwargs):
+    if parent is None:
+        raise KeyError('Cannot determine case without parent.')
+    return parent.parent.vertices_header.field_count > 65535
+
+def get_has_extra_block(parent=None, **kwargs):
+    if parent is None:
+        raise KeyError('Cannot determine case without parent.')
+    return math.isnan(parent.mult)
+
+def amf_newer_than_11(parent=None, **kwargs):
+    if parent.get_root().data.version > 1.1:
+        return True
+    return False
+    
 float_bounds = QStruct('bounds',
     Float('lower'), Float('upper'), ORIENT='h'
     )
 
-
 general_header = QStruct('header',
-    SInt32('count'),
-    SInt32('offset')
+    SInt32('field count', VISIBLE=False),
+    SInt32('array address', VISIBLE=False)
     )
-    
-amf_header = Container('amf header',
-    SInt32('header'), ## AMF! 0x21464D41
-    Float('version'), ## should max be 2.1
-    CStrAscii('model name'),
-    QStruct("vertices header", INCLUDE=general_header),
-    QStruct("markers header", INCLUDE=general_header),
-    QStruct("regions header", INCLUDE=general_header),
-    QStruct("shaders header", INCLUDE=general_header),
-    endian="<"
-    )
-    
     
 vertex_bounds = Struct('vertex bounds',
-    QStruct("x_bounds", INCLUDE=float_bounds),
-    QStruct("y_bounds", INCLUDE=float_bounds),
-    QStruct("z_bounds", INCLUDE=float_bounds),
-    QStruct("u_bounds", INCLUDE=float_bounds),
-    QStruct("v_bounds", INCLUDE=float_bounds)
+    QStruct('x_bounds', INCLUDE=float_bounds),
+    QStruct('y_bounds', INCLUDE=float_bounds),
+    QStruct('z_bounds', INCLUDE=float_bounds),
+    QStruct('u_bounds', INCLUDE=float_bounds),
+    QStruct('v_bounds', INCLUDE=float_bounds)
     )
     
-### node stuff
-
-## Comes right after header
-
-    
 node = Container('node',
-    CStrAscii('name'),
+    CStrUtf8('name'),
     SInt16('parent index'),
     SInt16('child index'),
     SInt16('sibling index'),
-    QStruct("position", INCLUDE=xyz_float),
-    QStruct("orientation", INCLUDE=ijkw_float),
-    endian="<"
-    )
+    QStruct('position', INCLUDE=xyz_float),
+    QStruct('orientation', INCLUDE=ijkw_float)
+)
     
+marker_instance = Struct('instance',
+    SInt8('region index'),
+    SInt8('parent index'), #parent? Not sure about this one
+    SInt16('node index'),
+    QStruct('position', INCLUDE=xyz_float),
+    QStruct('orientation', INCLUDE=ijkw_float)
+)
+    
+marker = Container('marker',
+    CStrUtf8('name'),
+    Struct('marker instances',
+        INCLUDE=general_header,
+        STEPTREE=Array('instances',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = marker_instance
+        )
+    )
+)
 
+vertex_compressed = Struct('vertex_compressed',
+    QStruct('position', SInt16('x', UNIT_SCALE=1/32767), SInt16('y', UNIT_SCALE=1/32767), SInt16('z', UNIT_SCALE=1/32767)),
+    BitStruct('normal', INCLUDE=compressed_normal_32, SIZE=4),
+    SInt16('u', UNIT_SCALE=1/32767),
+    SInt16('v', UNIT_SCALE=1/32767)
+)
+
+vertex_uncompressed = Struct('vertex_uncompressed',
+    QStruct('position', INCLUDE=xyz_float),
+    QStruct('normal', INCLUDE=ijk_float),
+    Float('u'),
+    Float('v')
+)
+
+vert_indices_1 = QStruct('vert_indices',
+    UInt8('node index 1'),
+    UInt8('node index sentinel', DEFAULT=255, VISIBLE=False)
+)
+vert_indices_2 = QStruct('vert_indices',
+    UInt8('node index 1'),
+    UInt8('node index 2'),
+    UInt8('node index sentinel', DEFAULT=255, VISIBLE=False)
+)
+vert_indices_3 = QStruct('vert_indices',
+    UInt8('node index 1'),
+    UInt8('node index 2'),
+    UInt8('node index 3'),
+    UInt8('node index sentinel', DEFAULT=255, VISIBLE=False)
+)
+vert_indices_4 = QStruct('vert_indices',
+    UInt8('node index 1'),
+    UInt8('node index 2'),
+    UInt8('node index 3'),
+    UInt8('node index 4'),
+)
+
+vert_weights_1 = QStruct('vert_weights',
+    Float('node weight 1'),
+)
+vert_weights_2 = QStruct('vert_weights',
+    Float('node weight 1'),
+    Float('node weight 2'),
+)
+vert_weights_3 = QStruct('vert_weights',
+    Float('node weight 1'),
+    Float('node weight 2'),
+    Float('node weight 3'),
+)
+vert_weights_4 = QStruct('vert_weights',
+    Float('node weight 1'),
+    Float('node weight 2'),
+    Float('node weight 3'),
+    Float('node weight 4'),
+)
+
+vertex_format_0 = Container('vertex',
+    Switch('data',
+        DEFAULT=vertex_uncompressed,
+        CASE='.....format_info.compression_format',
+        CASES={1: vertex_compressed,
+               2: vertex_compressed,
+               3: vertex_compressed}
+    )
+)
+
+vertex_format_1 = Container('vertex',
+    Switch('data',
+        DEFAULT=vertex_uncompressed,
+        CASE='.....format_info.compression_format',
+        CASES={1: vertex_compressed,
+               2: vertex_compressed,
+               3: vertex_compressed}
+    ),
+    Switch('node indices',
+        CASE=get_vert_node_count,
+        CASES={1: vert_indices_1,
+               2: vert_indices_2,
+               3: vert_indices_3,
+               4: vert_indices_4}
+    ),
+
+    Switch('weights',
+        CASE=get_vert_node_weight_count,
+        CASES={1: vert_weights_1,
+               2: vert_weights_2,
+               3: vert_weights_3,
+               4: vert_weights_4}
+    )
+)
+
+vertex_format_2 = Container('vertex',
+    Switch('data',
+        DEFAULT=vertex_uncompressed,
+        CASE='.....format_info.compression_format',
+        CASES={1: vertex_compressed,
+               2: vertex_compressed,
+               3: vertex_compressed}
+    ),
+    Switch('node indices',
+        CASE=get_vert_node_count,
+        CASES={1: vert_indices_1,
+               2: vert_indices_2,
+               3: vert_indices_3,
+               4: vert_indices_4}
+    )
+)
+
+vertex_format_0_array = Array('vertices array',
+    SIZE = '..field_count',
+    SUB_STRUCT=vertex_format_0
+)
+vertex_format_1_array = Array('vertices array',
+    SIZE = '..field_count',
+    SUB_STRUCT=vertex_format_1
+)
+vertex_format_2_array = Array('vertices array',
+    SIZE = '..field_count',
+    SUB_STRUCT=vertex_format_2
+)
+
+vertices = Container('vertices',
+    Switch('bounds',
+        CASE='...format_info.compression_format',
+        CASES={1: vertex_bounds}
+    ),
+    Switch('vertices',
+        CASE='...format_info.vertex_format',
+        CASES={0: vertex_format_0_array,
+               1: vertex_format_1_array,
+               2: vertex_format_2_array}
+    ),
+    POINTER='.array_address'
+)
+    
+two_byte_face = Container('face',
+    UInt16('v1'), UInt16('v2'), UInt16('v3')
+)
+
+four_byte_face = Container('face',
+    UInt32('v1'), UInt32('v2'), UInt32('v3')
+)
+
+two_byte_face_array = Array('faces',
+    SIZE = '.field_count',
+    SUB_STRUCT=two_byte_face
+)
+four_byte_face_array = Array('faces',
+    SIZE = '.field_count',
+    SUB_STRUCT=four_byte_face
+)
+    
+faces = Switch('faces',
+        CASE=is_vert_count_over_65535,
+        CASES={True: four_byte_face_array,
+               False: two_byte_face_array},
+        POINTER='.array_address'
+)
+
+set = Struct('set',
+    SInt16('shader index'),
+    SInt32('starting face'),
+    SInt32('face count')
+)
+
+sets = Array('sets',
+    SIZE = '.field_count',
+    SUB_STRUCT=set,
+    POINTER='.array_address'
+)
+
+transformation_matrix = Struct('transformation matrix',
+    QStruct('row1', INCLUDE=xyz_float),
+    QStruct('row2', INCLUDE=xyz_float),
+    QStruct('row3', INCLUDE=xyz_float),
+    QStruct('row4', INCLUDE=xyz_float)
+)
+
+part = Container('part',
+    CStrUtf8('name'),
+    BitStruct('format_info',
+        S1BitInt('vertex format', SIZE=4),
+        S1BitInt('compression format', SIZE=4)
+    ),
+    SInt8('some index'),
+    QStruct('vertices header',
+        INCLUDE=general_header,
+        STEPTREE=vertices
+    ),
+    Struct('faces header',
+        INCLUDE=general_header,
+        STEPTREE=faces
+    ),
+    Struct('set header',
+        INCLUDE=general_header,
+        STEPTREE=sets
+    ),
+    Float('mult'),
+    Switch('part_extension',
+        CASE=get_has_extra_block,
+        CASES={False: transformation_matrix}
+    )
+)
+
+region = Container('region',
+    CStrUtf8('name'),
+    Struct('parts header',
+        INCLUDE=general_header,
+        STEPTREE=Array('parts',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = part
+        )
+    )
+)
+    
+def is_path_valid(parent=None, **kwargs):
+    if parent.filepath != 'null':
+        return True
+    return False
+    
+bitmap = Container('bitmap',
+    CStrUtf8('filepath'),
+    Switch('tiling',
+        CASE=is_path_valid,
+        CASES={True: uv_float}
+    )
+)
+
+bitmap2 = Container('bitmap',
+    CStrUtf8('filepath'),
+    uv_float
+)
+
+normal_shader_colors = Array('colors',
+    SUB_STRUCT=argb_byte,
+    SIZE=4
+)
+
+normal_shader = Container('normal shader',
+    Array('bitmaps',
+        SUB_STRUCT=bitmap,
+        SIZE=8
+    ),
+    Switch('colors',
+        CASE=amf_newer_than_11,
+        CASES={True:normal_shader_colors}
+    ),
+    SInt8('is transparent'),
+    SInt8('cc only')
+)
+
+terrain_shader = Container('terrain shader',
+    Container('blend', INCLUDE=bitmap),
+    SInt8('basemap count'),
+    SInt8('bumpmap count'),
+    SInt8('detailmap count'),
+    Array('basemaps', SIZE='.basemap_count', SUB_STRUCT=bitmap2),
+    Array('bumpmaps', SIZE='.bumpmap_count', SUB_STRUCT=bitmap2),
+    Array('detailmaps', SIZE='.detailmap_count', SUB_STRUCT=bitmap2)
+)
+
+def name_starts_with_star(parent=None, **kwargs):
+    if parent.name[0] == '*':
+        parent.name = parent.name.replace('*', '')
+        return True
+    return False
+    
+shader = Container('shader',
+    CStrUtf8('name'),
+    Switch('type',
+        CASE=name_starts_with_star,
+        CASES={False:normal_shader,
+               True:terrain_shader}
+    )
+)
+    
     
 amf_def = TagDef('amf',
-    amf_header,
-    
-    Array('vertices',
-    SIZE = ".amf_header.vertices_header.count",
-    POINTER = ".amf_header.vertices_header.offset",
-    SUB_STRUCT = "node"
+    StrRawUtf8('header', SIZE = 4, DEFAULT='AMF!' ),
+    Float('version', DEFAULT=2.1), ## should max be 2.1
+    CStrUtf8('model name'),
+    Struct('nodes header',
+        INCLUDE=general_header,
+        STEPTREE=Array('nodes',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = node
+        )
     ),
-    
-    ext=".amf", endian="<"
-    )
+    Struct('markers header',
+        INCLUDE=general_header,
+        STEPTREE=Array('markers',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = marker
+        )
+    ),
+    Struct('regions header',
+        INCLUDE=general_header,
+        STEPTREE=Array('regions',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = region
+        )
+    ),
+    Struct('shaders header',
+        INCLUDE=general_header,
+        STEPTREE=Array('shaders',
+        SIZE = '.field_count',
+        POINTER = '.array_address',
+        SUB_STRUCT = shader
+        )
+    ),
+    ext='.amf', endian='<'
+)
     
 def get(): return amf_def
